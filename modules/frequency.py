c663fa83071420273e37f45fd53ce7774b1a1d67
Andrew Louis <andrew.louis93@gmail.com>
2015-10-07 16:38:05 -0400

    Added query counting module

---
+++ modules/frequency.py
@@ -0,0 +1,9 @@
+import re, collections
+def str_frequency(_string):
+    wordList = re.findall ('\w+',_string.lower())
+    frequency = collections.Counter(wordList).most_common()
+    return frequency
+if __name__ == "__main__":
+    test = "this is a a a a a a a test test"
+    print test
+    print wordFrequency(test)
